5a6993801fa37463bb9a37248cd66beb16c1b223
Haoji Xu <haoji.xu@outlook.com>
2017-11-29 23:43:37 +0800

    Phase 1 Completed: Howler.js√ Reading Detection√

--- src/components/preprocessing_chapter.js
+++ src/components/preprocessing_chapter.js
@@ -1,8 +1,39 @@
 exports.preprocess = function (original_md) {
 
-    var final_md = original_md + "<br\><audio controls><source src=\"https://upload.wikimedia.org/wikipedia/commons/f/fe/Internationale.ogg\" type=\"audio/ogg\"></audio>"
+        var final_md, control_obj = []
 
-    final_md = final_md + ""
+        return_arr = replace_music_embed(original_md)
+        final_md = return_arr[0]; control_obj["sounds"] = return_arr[1]
 
-    return final_md
+        return [final_md, control_obj]
+    }
+
+// += "<br/> [music src=\"https://upload.wikimedia.org/wikipedia/commons/f/fe/Internationale.ogg\"]"
+
+function replace_music_embed (text) {
+    // match pattern like: [music src=""]
+
+    const regex = /\[music\ src\=\"(.*)\"\]/g;
+    let m, matches = [];
+    var obj = [];
+
+    while ((m = regex.exec(text)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+
+        matches.push([m, m.index, m.index + m[0].length])
+        text = text.substring(0, m.index) + "<div id='detector'></div>" + text.substring(m.index + m[0].length);
+
+        //var urlPattern = /(([A-Za-z]{3,9}):\/\/)?([-;:&=\+\$,\w]+@{1})?(([-A-Za-z0-9]+\.)+[A-Za-z]{2,3})(:\d+)?((\/[-\+~%/\.\w]+)?\/?([&?][-\+=&;%@\.\w]+)?(#[\w]+)?)?/g
+        //var soundUrl = urlPattern.exec(m)
+        obj.push(m[1])
+        console.log(m[1])
+
+        //script += "console.log('Hello., From Preprocessing')"
+    }
+
+
+    return [text, obj]
 }
--- src/pages/view.vue
+++ src/pages/view.vue
@@ -5,7 +5,7 @@
         <!-- The Chapter Title -->
 
                 <v-card>
-                    <v-parallax src="https://images.apple.com/v/mac/home/x/images/home/business_updated_large_2x.jpg" height="600">
+                    <v-parallax src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Universit%C3%A9_Paris_1_Panth%C3%A9on_-_Sorbonne._Entr%C3%A9e_du_Centre_Panth%C3%A9on_%281%29.JPG/1200px-Universit%C3%A9_Paris_1_Panth%C3%A9on_-_Sorbonne._Entr%C3%A9e_du_Centre_Panth%C3%A9on_%281%29.JPG" height="600">
                         <v-layout column align-center justify-center>
                             <h2 class="black--text"> {{chapterInfo.Title}} </h2>
                             <a class="black--text">
@@ -19,14 +19,22 @@
                 </v-card>
 
 
-        <chapter_viewer :display="preprocess(chapterInfo.Content)"></chapter_viewer>
+        <v-container
+                style="min-height: 0;"
+                grid-list-lg
+        >
+
+
+            <vue-markdown :source="display" style="max-width: 70%; margin: auto; font-size: inherit" typographer:false toc:true class="rendered_view"></vue-markdown>
+
+
+        </v-container>
 
         <v-layout row wrap>
             <v-flex xs6 offset-xs3>
                 <div id="disqus_thread"></div>
             </v-flex>
         </v-layout>
-
 
 
         <footerbar></footerbar>
@@ -36,22 +44,56 @@
 <script>
     import headerbar from '../components/global/header.vue'
     import footerbar from '../components/global/footer.vue'
-    import chapter_viewer from '../components/chapter_viewer.vue'
 
     import utils from '../components/preprocessing_chapter.js'
-
-    //import {APIGetSingleChapter} from '../../config.js'
+    import VueMarkdown from 'vue-markdown'
+    //import howler from  'https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.5/howler.js'
 
     export default {
         name: 'app',
         components: {
-            headerbar, footerbar, chapter_viewer
+            headerbar, footerbar, VueMarkdown
         },
 
         data () {
             return {
                 chapterInfo: [],
-                preprocess: utils.preprocess
+                preprocess: utils.preprocess,
+                display: "",
+                control_script: "",
+                toHeight: 0,
+                fromHeight: 0,
+                current_sound: 0,
+                sounds: null
+            }
+        },
+
+        methods: {
+            loadScripts() {
+                return new Promise(resolve => {
+
+                    let scriptEl = document.createElement("script");
+                    scriptEl.src = "https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.5/howler.js";
+                    scriptEl.type = "text/javascript";
+
+                    // Attach script to head
+                    document.getElementsByTagName("head")[0].appendChild(scriptEl);
+                    // Wait for tag to load before promise is resolved
+                    scriptEl.addEventListener('load',() => {
+                        resolve();
+                    });
+                });
+
+            },
+            handleScroll () {
+                //this. = window.scrollY > 0;
+                this.toHeight = document.getElementById("detector").offsetTop
+                this.fromHeight = window.pageYOffset
+                if(this.toHeight - this.fromHeight < 30 && this.toHeight - this.fromHeight > 0) {
+                    if (this.sounds[this.current_sound] != null)
+                        var sound = new Howl({src: this.sounds[this.current_sound]}).play();
+                    this.current_sound += 1;
+                }
             }
         },
         created: function () {
@@ -59,10 +101,18 @@
 
             var vm = this;
             var xmlHttp = new XMLHttpRequest();
+            vm.loadScripts();
+
             xmlHttp.onreadystatechange = function () {
-                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                    vm.chapterInfo = JSON.parse(xmlHttp.responseText)[0]
-                console.log(vm.chapterInfo)
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    vm.chapterInfo = JSON.parse(xmlHttp.responseText)[0];
+                    console.log(vm.chapterInfo)
+
+                    var temp_arr = utils.preprocess(vm.chapterInfo.Content)
+                    vm.display = temp_arr[0]
+                    vm.sounds = temp_arr[1]["sounds"]
+                    vm.current_sound = 0
+                }
             }
             xmlHttp.open("GET", APIGetSingleChapter + vm.$router.currentRoute.query.titleID, true);
             xmlHttp.send(null);
@@ -71,15 +121,21 @@
             //vm.chapterInfo.Content += "<br\><audio controls autoplay><source src=\"https://upload.wikimedia.org/wikipedia/commons/c/c3/Antonin_Dvorak_-_symphony_no._9_in_e_minor_%27from_the_new_world%27%2C_op._95_-_ii._largo.ogg\" type=\"audio/ogg\"></audio>"
             //vm.chapterInfo.Content = ""
 
-            // Now getting work data
-            // TODO
-
             // Load Disquz
             var d = document, s = d.createElement('script');
             s.src = 'https://project-siren.disqus.com/embed.js';
             s.setAttribute('data-timestamp', +new Date());
             (d.head || d.body).appendChild(s);
+
+            // Create event handlers
+            window.addEventListener('scroll', this.handleScroll);
+        },
+
+        destroyed () {
+            window.removeEventListener('scroll', this.handleScroll);
         }
+
+
     }
 
 </script>
